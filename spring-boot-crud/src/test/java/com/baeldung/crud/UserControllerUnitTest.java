50bbb10bb2a9fe41f6d05bd70ebe24ec19e0fcff
Phill Conrad <pconrad@cs.ucsb.edu>
2019-11-04 16:06:56 -0800

    pc - add controller tests back in

--- spring-boot-crud/src/test/java/com/baeldung/crud/UserControllerUnitTest.java
+++ spring-boot-crud/src/test/java/com/baeldung/crud/UserControllerUnitTest.java
@@ -20,64 +20,64 @@ public class UserControllerUnitTest {
     private static BindingResult mockedBindingResult;
     private static Model mockedModel;
 
-    // @BeforeClass
-    // public static void setUpUserControllerInstance() {
-    //     mockedUserRepository = mock(UserRepository.class);
-    //     mockedBindingResult = mock(BindingResult.class);
-    //     mockedModel = mock(Model.class);
-    //     userController = new UserController(mockedUserRepository);
-    // }
+    @BeforeClass
+    public static void setUpUserControllerInstance() {
+        mockedUserRepository = mock(UserRepository.class);
+        mockedBindingResult = mock(BindingResult.class);
+        mockedModel = mock(Model.class);
+        userController = new UserController(mockedUserRepository);
+    }
 
-    // @Test
-    // public void whenCalledshowSignUpForm_thenCorrect() {
-    //     User user = new User("John", "john@domain.com");
+    @Test
+    public void whenCalledshowSignUpForm_thenCorrect() {
+        User user = new User("John", "Smith", "john@domain.com");
 
-    //     assertThat(userController.showSignUpForm(user)).isEqualTo("add-user");
-    // }
+        assertThat(userController.showSignUpForm(user)).isEqualTo("add-user");
+    }
     
-    // @Test
-    // public void whenCalledaddUserAndValidUser_thenCorrect() {
-    //     User user = new User("John", "john@domain.com");
+    @Test
+    public void whenCalledaddUserAndValidUser_thenCorrect() {
+        User user = new User("John", "Smith", "john@domain.com");
 
-    //     when(mockedBindingResult.hasErrors()).thenReturn(false);
+        when(mockedBindingResult.hasErrors()).thenReturn(false);
 
-    //     assertThat(userController.addUser(user, mockedBindingResult, mockedModel)).isEqualTo("index");
-    // }
+        assertThat(userController.addUser(user, mockedBindingResult, mockedModel)).isEqualTo("index");
+    }
 
-    // @Test
-    // public void whenCalledaddUserAndInValidUser_thenCorrect() {
-    //     User user = new User("John", "john@domain.com");
+    @Test
+    public void whenCalledaddUserAndInValidUser_thenCorrect() {
+        User user = new User("John", "Smith", "john@domain.com");
 
-    //     when(mockedBindingResult.hasErrors()).thenReturn(true);
+        when(mockedBindingResult.hasErrors()).thenReturn(true);
 
-    //     assertThat(userController.addUser(user, mockedBindingResult, mockedModel)).isEqualTo("add-user");
-    // }
+        assertThat(userController.addUser(user, mockedBindingResult, mockedModel)).isEqualTo("add-user");
+    }
 
-    // @Test(expected = IllegalArgumentException.class)
-    // public void whenCalledshowUpdateForm_thenIllegalArgumentException() {
-    //     assertThat(userController.showUpdateForm(0, mockedModel)).isEqualTo("update-user");
-    // }
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCalledshowUpdateForm_thenIllegalArgumentException() {
+        assertThat(userController.showUpdateForm(0, mockedModel)).isEqualTo("update-user");
+    }
     
-    // @Test
-    // public void whenCalledupdateUserAndValidUser_thenCorrect() {
-    //     User user = new User("John", "john@domain.com");
+    @Test
+    public void whenCalledupdateUserAndValidUser_thenCorrect() {
+        User user = new User("John", "Smith", "john@domain.com");
 
-    //     when(mockedBindingResult.hasErrors()).thenReturn(false);
+        when(mockedBindingResult.hasErrors()).thenReturn(false);
 
-    //     assertThat(userController.updateUser(1l, user, mockedBindingResult, mockedModel)).isEqualTo("index");
-    // }
+        assertThat(userController.updateUser(1l, user, mockedBindingResult, mockedModel)).isEqualTo("index");
+    }
 
-    // @Test
-    // public void whenCalledupdateUserAndInValidUser_thenCorrect() {
-    //     User user = new User("John", "john@domain.com");
+    @Test
+    public void whenCalledupdateUserAndInValidUser_thenCorrect() {
+        User user = new User("John", "Smith", "john@domain.com");
 
-    //     when(mockedBindingResult.hasErrors()).thenReturn(true);
+        when(mockedBindingResult.hasErrors()).thenReturn(true);
 
-    //     assertThat(userController.updateUser(1l, user, mockedBindingResult, mockedModel)).isEqualTo("update-user");
-    // }
+        assertThat(userController.updateUser(1l, user, mockedBindingResult, mockedModel)).isEqualTo("update-user");
+    }
     
-    // @Test(expected = IllegalArgumentException.class)
-    // public void whenCalleddeleteUser_thenIllegalArgumentException() {
-    //     assertThat(userController.deleteUser(1l, mockedModel)).isEqualTo("index");
-    // }
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCalleddeleteUser_thenIllegalArgumentException() {
+        assertThat(userController.deleteUser(1l, mockedModel)).isEqualTo("index");
+    }
 }
